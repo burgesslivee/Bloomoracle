9a5ab0791d5eec6436c10a6876491c1886753620
burgesslivee <burgesslivee@gmail.com>
2026-02-27 18:42:52 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BloomOracle from './BloomOracle.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BloomOracle />
+  </React.StrictMode>,
+)
